8f501c0e101c16cc342c4722ff9e6294859e71cd
Antonio Barbosa <acrb1108@gmail.com>
2024-10-04 23:14:31 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoEmprestimoLivrosCurso/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoEmprestimoLivrosCurso.Models;
+using System.Diagnostics;
+
+namespace ProjetoEmprestimoLivrosCurso.Controllers;
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ ProjetoEmprestimoLivrosCurso/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProjetoEmprestimoLivrosCurso.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
