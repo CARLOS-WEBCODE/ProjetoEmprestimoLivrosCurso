25dc98d610b0ff375260e647a787b50724115885
Antonio Barbosa <acrb1108@gmail.com>
2024-10-04 23:22:11 -0300

    Ajustando as pastas

--- ProjetoEmprestimoLivrosCurso/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ProjetoEmprestimoLivrosCurso.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
